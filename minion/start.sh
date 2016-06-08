1db0e14f3f97ec59cf582dabb382507cbfee3a23
Matt Willsher <matt@bipsync.com>
2016-06-08 14:32:14 +0100

    Seperate images

---
+++ minion/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+[ -d /etc/salt/minion.d ] || mkdir /etc/salt/minion.d
+[ -f /etc/salt/minion ] || cp /root/minion /etc/salt/minion
+[ -f /etc/salt/minion.d/default.conf ] || echo master: localhost >/etc/salt/minion.d/default.conf
+
+exec salt-minion -c /etc/salt
