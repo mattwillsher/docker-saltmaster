8462b92317b5b3f91ea6b5fc292665baa3a081ca
Matt Willsher <matt@bipsync.com>
2016-06-08 16:04:12 +0100

    Add compose

--- minion/start.sh
+++ minion/start.sh
@@ -2,6 +2,6 @@
 
 [ -d /etc/salt/minion.d ] || mkdir /etc/salt/minion.d
 [ -f /etc/salt/minion ] || cp /root/minion /etc/salt/minion
-[ -f /etc/salt/minion.d/default.conf ] || echo master: localhost >/etc/salt/minion.d/default.conf
+[ -f /etc/salt/minion.d/default.conf ] || echo master: ${master} >/etc/salt/minion.d/default.conf
 
 exec salt-minion -c /etc/salt
